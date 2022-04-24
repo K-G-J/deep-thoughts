920bbbf31c3cf9aca4676749c80f6958ca55dabe
Kate Johnson <johnson.kat.g@gmail.com>
2022-04-23 23:45:20 -0400

    added thought and reaction forms

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 import { QUERY_THOUGHT } from '../utils/queries'
 import ReactionList from '../components/ReactionList'
+import ReactionForm from '../components/ReactionForm'
+import Auth from '../utils/auth'
 
 export default function SingleThought() {
   const { id: thoughtId } = useParams();
@@ -34,6 +36,7 @@ export default function SingleThought() {
       {thought.reactionCount > 0 && (
         <ReactionList reactions={thought.reactions} />
       )}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id}/>}
     </div>
   );
 }
